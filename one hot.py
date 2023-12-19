14f433a3772a08a7be878e9b08215520eb40aff3
Alina <alinarepinaja@gmail.com>
2023-12-19 18:06:27 +0400

    attestation

---
+++ one hot.py
@@ -0,0 +1,23 @@
+
+# import pandas as pd
+#
+# data = pd.Series([1, 2, 3])
+# print(data)
+
+
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+#print(data.head())
+
+one_hot_alt = pd.DataFrame()
+one_hot_alt["human"] = (data['whoAmI'] == 'human').astype(int)
+one_hot_alt["robot"] = (data['whoAmI'] == 'robot').astype(int)
+
+#one_hot_alt = pd.get_dummies(data['whoAmI']).astype(int)
+
+print(one_hot_alt.head())
